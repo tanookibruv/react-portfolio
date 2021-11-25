27d35179b07464e8a7d45412fb0270bb5563596b
tanookibruv <erendon1836@gmail.com>
2021-11-24 22:33:24 -0800

    setting up routes

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/about';
+import Work from './pages/work';
+import Contact from './pages/contact';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/work">
+        <Work />
+      </Route>
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
