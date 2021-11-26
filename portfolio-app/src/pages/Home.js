4944cb56eb6a481fe6457f4ab1900f9288c5ed49
tanookibruv <erendon1836@gmail.com>
2021-11-26 12:56:45 -0800

    getting started on the home page

---
+++ portfolio-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { link } from 'react-router-dom';
+import 'react-bootstrap';
+import avatar from '../assets/logos/TempAvatar.png'
+
+const Home = () => {
+    console.log("Hey, this is the Home page!")
+
+    return (
+        <main>
+            
+        </main>
+    )
+}
