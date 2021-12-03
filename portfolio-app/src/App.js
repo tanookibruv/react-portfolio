648794345e48f09a83b02b3e94313d6ff91e477b
tanookibruv <erendon1836@gmail.com>
2021-12-02 23:32:51 -0800

    using react router dom V6

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -4,26 +4,20 @@ import Home from './pages/Home';
 import About from './pages/about';
 import Work from './pages/work';
 import Contact from './pages/contact';
-// import Header from './components/header';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
   <Router>
-    {/* <Header /> */}
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/work">
-        <Work />
-      </Route>
-      <Route exact path="/contact">
-        <Contact />
-      </Route>
-    </Switch>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/work" element={<Work/>}/>
+      <Route exact path="/contact" element={<Contact/>}/>
+    </Routes>
+    <Footer />
   </Router>
   );
 }
