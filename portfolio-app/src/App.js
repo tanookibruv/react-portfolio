b7b17469ad1dbdcbcf5ad33d984fe05394784cf0
tanookibruv <erendon1836@gmail.com>
2021-12-09 21:46:41 -0800

    working on pages

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
+import Home from './pages/home';
 import About from './pages/about';
 import Work from './pages/work';
 import Contact from './pages/contact';
